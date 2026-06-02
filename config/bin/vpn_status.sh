6f1f5c8b34458ae324119d77896c4c63359833ac
HTB Prometeo <142384720+HTBPrometeo@users.noreply.github.com>
2026-06-01 20:27:28 -0600

    Update vpn_status.sh

--- config/bin/vpn_status.sh
+++ config/bin/vpn_status.sh
@@ -3,7 +3,7 @@
 IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
  
 if [ "$IFACE" = "tun0" ]; then
-    echo "%{F#A182FF} %{F#ffffff}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}"
+    echo "%{F#0077B6} %{F#ffffff}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}"
 else
-    echo "%{F#A182FF} %{u-} Disconnected"
+    echo "%{F#0077B6} %{u-} Disconnected"
 fi
