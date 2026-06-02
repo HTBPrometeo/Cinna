7d10f38ec35c9970f12a63eaee75dc075646395a
HTB Prometeo <142384720+HTBPrometeo@users.noreply.github.com>
2026-06-01 22:21:56 -0600

    Update target_to_hack.sh

--- config/bin/target_to_hack.sh
+++ config/bin/target_to_hack.sh
@@ -4,7 +4,7 @@ ip_address=$(/bin/cat ~/.config/bin/target | awk '{print $1}')
 machine_name=$(/bin/cat ~/.config/bin/target | awk '{print $2}')
  
 if [ $ip_address ] && [ $machine_name ]; then
-    echo "%{F#0077B6}ﲅ %{F#ffffff}$ip_address%{u-} - $machine_name"
+    echo "%{F#4AC6FF}ﲅ %{F#ffffff}$ip_address%{u-} - $machine_name"
 else
-    echo "%{F#0077B6}ﲅ %{u-}%{F#ffffff} No target"
+    echo "%{F#4AC6FF}ﲅ %{u-}%{F#ffffff} No target"
 fi
